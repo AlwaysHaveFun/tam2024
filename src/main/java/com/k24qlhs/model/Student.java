b45d4da618b16545f3c18eca77f0cf939804804c
AlwaysHaveFun <nguyenductam.dev@gmail.com>
2024-02-15 14:03:57 +0700

    first commit

---
+++ src/main/java/com/k24qlhs/model/Student.java
@@ -0,0 +1,16 @@
+package com.k24qlhs.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Student {
+    private int sid;
+    private String sname;
+    private int cid;
+
+}
